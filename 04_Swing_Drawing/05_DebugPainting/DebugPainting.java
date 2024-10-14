f38f6805ad87f7ca1deb18f2354fa9a000d48e6d
Rutar Andriy <RutarAndriy@gmail.com>
2024-10-14 22:36:03 +0300

    Added example 05 - Debug_Painting

--- 04_Swing_Drawing/05_DebugPainting/DebugPainting.java
+++ 04_Swing_Drawing/05_DebugPainting/DebugPainting.java
@@ -1,33 +1,34 @@
 // DebugPainting.java
-// Демонстрация возможностей отладки графики в Swing
+// Демонстрація можливостей налагоджування графіки в Swing
 import java.awt.*;
 import javax.swing.*;
 
 public class DebugPainting extends JFrame {
   public DebugPainting() {
     super("DebugPainting");
-    // выход при закрытии окна
+    // вихід при закриванні вікна
     setDefaultCloseOperation(EXIT_ON_CLOSE);
-    // добавляем рисующий компонент
+    // додаємо малюючий компонент
     PaintingComponent pc = new PaintingComponent();
     add(pc);
-    // включаем для него отладку графики
+    // вмикаємо для нього налагоджування графіки
     RepaintManager.currentManager(pc).
         setDoubleBufferingEnabled(false);
     pc.setDebugGraphicsOptions(DebugGraphics.LOG_OPTION
-        | DebugGraphics.FLASH_OPTION);
-    DebugGraphics.setFlashTime(50);
+                             | DebugGraphics.FLASH_OPTION);
+    DebugGraphics.setFlashTime(100);
     DebugGraphics.setFlashCount(3);
-    // выводим окно на экран
+    DebugGraphics.setFlashColor(Color.GRAY);
+    // виводимо вікно на екран
     setSize(200, 200);
     setVisible(true);
   }
 
-  // компонент, который что-то рисует
+  // компонент, який що-небуть малює
   class PaintingComponent extends JPanel {
     public void paintComponent(Graphics g) {
       super.paintComponent(g);
-      // три простые фигуры
+      // три прості фігури
       g.setColor(Color.orange);
       g.fillRect(10, 10, 100, 100);
       g.setColor(Color.green);
