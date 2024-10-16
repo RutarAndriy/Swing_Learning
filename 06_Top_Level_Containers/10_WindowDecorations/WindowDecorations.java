a618ee42fe10cc3b5e9afb5d60552109d0d4f17b
Rutar Andriy <RutarAndriy@gmail.com>
2024-10-16 10:59:54 +0300

    Added example 10 - Window Decorations

--- 06_Top_Level_Containers/10_WindowDecorations/WindowDecorations.java
+++ 06_Top_Level_Containers/10_WindowDecorations/WindowDecorations.java
@@ -1,5 +1,5 @@
 // WindowDecorations.java
-// Специальное оформление окон Swing
+// Спеціальне оформлення вікон Swing
 import javax.swing.*;
 
 public class WindowDecorations {
@@ -7,21 +7,21 @@ public class WindowDecorations {
     SwingUtilities.invokeLater(
         new Runnable() {
           public void run() {
-            // включим украшения для окон
+            // включимо декорації для вікон
             JFrame.setDefaultLookAndFeelDecorated(true);
             JDialog.setDefaultLookAndFeelDecorated(true);
-            // окно с рамкой
-            JFrame frame = new JFrame("Окно с рамкой");
+            // вікно з рамкою
+            JFrame frame = new JFrame("Вікно з рамкою");
             frame.setDefaultCloseOperation(
                 JFrame.EXIT_ON_CLOSE);
             frame.setSize(200, 200);
             frame.setVisible(true);
-            // диалоговое окно
-            JDialog dialog = new JDialog(frame, "Диалог");
+            // діалогове вікно
+            JDialog dialog = new JDialog(frame, "Діалог");
             dialog.setDefaultCloseOperation(
                 JDialog.DISPOSE_ON_CLOSE);
             dialog.setSize(150, 100);
-            // так можно задавать тип оформления окна
+            // так можна задавати тип оформлення вікна
             dialog.getRootPane().setWindowDecorationStyle(
                 JRootPane.INFORMATION_DIALOG);
             dialog.setVisible(true);
