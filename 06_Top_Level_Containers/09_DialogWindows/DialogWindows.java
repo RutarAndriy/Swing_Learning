4cb42a120a17dfb51b04792b77cf56ea948b4b2b
Rutar Andriy <RutarAndriy@gmail.com>
2024-10-16 10:08:55 +0300

    Added example 09 - Dialog Windows

--- 06_Top_Level_Containers/09_DialogWindows/DialogWindows.java
+++ 06_Top_Level_Containers/09_DialogWindows/DialogWindows.java
@@ -1,5 +1,5 @@
 // DialogWindows.java
-// Диалоговые окна в Swing
+// Діалогові вікна в Swing
 import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
@@ -7,28 +7,28 @@ import java.awt.*;
 public class DialogWindows extends JFrame {
   public DialogWindows() {
     super("DialogWindows");
-    // выход при закрытии
+    // вихід при закритті
     setDefaultCloseOperation(EXIT_ON_CLOSE);
-    // пара кнопок, вызывающих создание диалоговых окон
-    JButton button1 = new JButton("Обычное окно");
+    // пара кнопок, для створення діалогових вікон
+    JButton button1 = new JButton("Звичайне вікно");
     button1.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
         JDialog dialog =
-          createDialog("Немодальное",
+          createDialog("Немодальне",
               Dialog.ModalityType.MODELESS);
         dialog.setVisible(true);
       }
       });
-    JButton button2 = new JButton("Модальное окно");
+    JButton button2 = new JButton("Модальне вікно");
     button2.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
         JDialog dialog =
-          createDialog("Модальное",
+          createDialog("Модальне",
               Dialog.ModalityType.APPLICATION_MODAL);
         dialog.setVisible(true);
       }
       });
-    // создаем панель содержимого и выводим окно на экран
+    // створюємо панель вмісту та виводимо вікно на екран
     JPanel contents = new JPanel();
     contents.add(button1);
     contents.add(button2);
@@ -36,7 +36,7 @@ public class DialogWindows extends JFrame {
     setSize(350, 100);
     setVisible(true);
   }
-  // создает диалоговое окно
+  // створює діалогове вікно
   private JDialog createDialog(String title, Dialog.ModalityType modal){
     JDialog dialog = new JDialog(this, title, modal);
     dialog.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
