04c3b94b6f4b67cf7456eb8ba3ade76c3825312c
Rutar Andriy <RutarAndriy@gmail.com>
2024-10-17 11:05:25 +0300

    Added example 06 - Spring_Layout_Sample

--- 07_Layout_Managers/06_SpringLayoutSample/SpringLayoutSample.java
+++ 07_Layout_Managers/06_SpringLayoutSample/SpringLayoutSample.java
@@ -1,5 +1,5 @@
 // SpringLayoutSample.java
-// Работа менеджера SpringLayout
+// Робота менеджера SpringLayout
 import javax.swing.*;
 import java.awt.*;
 
@@ -7,19 +7,19 @@ public class SpringLayoutSample extends JFrame {
 	public SpringLayoutSample() {
 		super("SpringLayoutSample");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		// панель с использованием SpringLayout
+		// панель з використанням SpringLayout
 		SpringLayout sl = new SpringLayout();
 		JPanel contents = new JPanel(sl);		
-		// добавим пару компонентов
+		// додамо декілька компонентів
     JButton button1, button2;
-		contents.add(button1 = new JButton("Первая"));
-		contents.add(button2 = new JButton("Последняя"));
-		// настроим распорки
+		contents.add(button1 = new JButton("Перша"));
+		contents.add(button2 = new JButton("Остання"));
+		// налаштуємо відступи
 		sl.putConstraint(SpringLayout.WEST, button1, 
 			5, SpringLayout.WEST, contents);
 		sl.putConstraint(SpringLayout.WEST, button2, 5,
 			SpringLayout.EAST, button1);		
-		// выведем окно на экран
+		// виводимо вікно на екран
 		setContentPane(contents);
 		setSize(300, 200);
 		setVisible(true);
