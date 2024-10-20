e3b75fd1080725530ec71f5c1a56a9a5ab0c6d43
Rutar Andriy <RutarAndriy@gmail.com>
2024-10-20 12:42:22 +0300

    Added example 06 - Borders

--- 08_Auxiliary_Information/06_Borders/Borders.java
+++ 08_Auxiliary_Information/06_Borders/Borders.java
@@ -8,11 +8,11 @@ public class Borders extends JFrame {
   public Borders() {
     super("Borders");
     setDefaultCloseOperation(EXIT_ON_CLOSE);
-    // создаем панели с всевозможными рамками
+    // створимо панелі із всеможливими рамками
     JPanel contents = new JPanel(
         new GridLayout(3, 2, 5, 5));
     contents.add(createPanel(new TitledBorder(
-        "Рамка с заголовком"), "TitledBorder"));
+        "Рамка із заголовком"), "TitledBorder"));
     contents.add(createPanel(new EtchedBorder(),
         "EtchedBorder"));
     contents.add(createPanel(new BevelBorder(
@@ -22,13 +22,13 @@ public class Borders extends JFrame {
     contents.add(createPanel(new LineBorder(
         Color.BLACK, 5), "LineBorder"));
     contents.add(createPanel(new MatteBorder(
-        new ImageIcon("matte.gif")), "MatteBorder"));
-    // выводим окно на экран
+        new ImageIcon("Matte.gif")), "MatteBorder"));
+    // виводимо вікно на екран
     add(contents);
     pack();
     setVisible(true);
   }
-  // метод создает панель с рамкой и надписью
+  // метод створює панель з рамкою та написом
   private JPanel createPanel(Border b, String text) {
     JPanel panel = new JPanel(new BorderLayout());
     panel.add(new JLabel(text));
