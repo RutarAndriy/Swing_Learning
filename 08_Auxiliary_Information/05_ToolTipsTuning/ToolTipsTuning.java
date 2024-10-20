d34fffb7c44b3ad581f10205b8001fbbac061e5b
Rutar Andriy <RutarAndriy@gmail.com>
2024-10-20 12:32:24 +0300

    Added example 05 - Tool_Tips_Tuning

--- 08_Auxiliary_Information/05_ToolTipsTuning/ToolTipsTuning.java
+++ 08_Auxiliary_Information/05_ToolTipsTuning/ToolTipsTuning.java
@@ -1,27 +1,27 @@
 // ToolTipsTuning.java
-// Настройка подсказок
+// Налаштування підказок
 import javax.swing.*;
 
 public class ToolTipsTuning extends JFrame {
   public ToolTipsTuning() {
     super("ToolTipsTuning");
     setDefaultCloseOperation(EXIT_ON_CLOSE);
-    // добавим кнопки с подсказками
+    // додамо кнопки з підказками
     JPanel contents = new JPanel();
-    JButton b1 = new JButton("Первая");
-    b1.setToolTipText("Подсказка для первой");
-    JButton b2 = new JButton("Вторая");
-    b2.setToolTipText("Подсказка для второй");
+    JButton b1 = new JButton("Перша");
+    b1.setToolTipText("Підказка для першої");
+    JButton b2 = new JButton("Друга");
+    b2.setToolTipText("Підказка для другої");
     contents.add(b1);
     contents.add(b2);
-    // настройка подсказок
+    // налаштування підказок
     ToolTipManager ttm =
         ToolTipManager.sharedInstance();
     ttm.setLightWeightPopupEnabled(false);
     ttm.setInitialDelay(1000);
     ttm.setDismissDelay(500);
     ttm.setReshowDelay(1000);
-    // выводим окно на экран
+    // виводимо вікно на екран
     add(contents);
     setSize(200, 100);
     setVisible(true);
