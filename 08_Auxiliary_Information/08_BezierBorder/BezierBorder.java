0c5d678254f4e4d527325ae3a56eec5ee32ad911
Rutar Andriy <RutarAndriy@gmail.com>
2024-10-20 12:57:36 +0300

    Added example 08 - Bazier_Border

--- 08_Auxiliary_Information/08_BezierBorder/BezierBorder.java
+++ 08_Auxiliary_Information/08_BezierBorder/BezierBorder.java
@@ -1,16 +1,16 @@
 // BezierBorder.java
-// Рамка, составленная из кривых Безье
+// Рамка, складена із кривих Безьє
 import javax.swing.*;
 import javax.swing.border.*;
 import java.awt.*;
 import java.awt.geom.*;
 
 public class BezierBorder implements Border {
-  // свойства рамки
+  // властивості рамки
   private Color color, shadow;
   private float thickness;
 
-  // значения свойств передаются в конструкторе
+  // значення властивостей передаються в конструкторі
   public BezierBorder(Color color, Color shadow,
                       float thickness) {
     this.color = color;
@@ -18,26 +18,26 @@ public class BezierBorder implements Border {
     this.thickness = thickness;
   }
 
-  // место, занимаемое рамкой
+  // місце, яке займає рамка
   public Insets getBorderInsets(Component comp) {
     return new Insets(9, 9, 9, 9);
   }
-  // наша рамка частями прозрачна
+  // наша рамка частково прозора
   public boolean isBorderOpaque() {
     return false;
   }
-  // метод прорисовки рамки
+  // метод промальовування рамки
   public void paintBorder(Component c, Graphics g,
                           int x, int y, int width, int height) {
-    // используем новый объект Graphics
+    // використовуємо новий об'єкт Graphics
     Graphics2D g2 = (Graphics2D)g.create();
-    // настройка пера, координат и цвета
+    // налаштування пера, координат та кольору
     g2.setStroke(new BasicStroke(thickness));
     g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
         RenderingHints.VALUE_ANTIALIAS_ON);
     x += 5; y += 5; width -= 10; height -= 10;
     g2.setColor(shadow);
-    // прорисовываем тень и рамку
+    // промальовуємо тінь та рамку
     for (int i=0; i<2; i++) {
       CubicCurve2D left = new CubicCurve2D.Double( x, y,
           x-5, y+height*1/3, x+5, y+height*1/3,
@@ -54,7 +54,7 @@ public class BezierBorder implements Border {
       g2.draw(right);
       g2.draw(top);
       g2.draw(bottom);
-      // на втором шаге рисуем саму рамку
+      // на другому кроці малюємо саму рамку
       x--; y--; width--; height--;
       g2.setColor(color);
     }
@@ -66,13 +66,13 @@ public class BezierBorder implements Border {
         new Runnable() {
           public void run() {
             JFrame frame = new JFrame("BezierBorder");
-            // создаем панель с нашей рамкой
+            // створюємо панель з нашою рамкою
             JPanel p = new JPanel(new BorderLayout());
             Border b = new TitledBorder(new BezierBorder(
                 Color.GREEN, Color.DARK_GRAY, 3f), "Bezier");
             p.setBorder(b);
             p.add(new JTextArea());
-            // выводим окно на экран
+            // виводимо вікно на екран
             frame.setDefaultCloseOperation(
                 JFrame.EXIT_ON_CLOSE);
             frame.add(p);
