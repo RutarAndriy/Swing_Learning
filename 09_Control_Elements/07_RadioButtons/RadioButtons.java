749461841c2736f79ac8565013a1b3ea97be4937
Rutar Andriy <RutarAndriy@gmail.com>
2024-10-21 10:31:09 +0300

    Added example 07 - Rabio_Buttons

--- 09_Control_Elements/07_RadioButtons/RadioButtons.java
+++ 09_Control_Elements/07_RadioButtons/RadioButtons.java
@@ -1,5 +1,5 @@
 // RadioButtons.java
-// Использование переключателей
+// Використання перемикачів
 import javax.swing.*;
 import java.awt.*;
 
@@ -7,27 +7,27 @@ public class RadioButtons extends JFrame {
   public RadioButtons() {
     super("RadioButtons");
     setDefaultCloseOperation( EXIT_ON_CLOSE );
-    // используем последовательное расположение
+    // використовуємо послідовне розміщення
     setLayout(new FlowLayout());
-    // отдельный переключатель
-    JRadioButton r = new JRadioButton("Сам по себе");
-    // группа связанных переключателей в своей
-    // собственной панели
+    // окремий перемикач
+    JRadioButton r = new JRadioButton("Сам по собі");
+    // група зв'язаних перемикачів у своїй
+    // власній панелі
     JPanel panel = new JPanel(new GridLayout(0, 1, 0, 5));
     panel.setBorder(
-        BorderFactory.createTitledBorder("Внешний вид"));
+        BorderFactory.createTitledBorder("Зовнішній вигляд"));
     ButtonGroup bg = new ButtonGroup();
-    String[] names = { "Внешний вид Java",
+    String[] names = { "Зовнішній вигляд Java",
         "MS Windows", "Aqua (Mac)" };
     for (String name : names) {
       JRadioButton radio = new JRadioButton(name);
       panel.add(radio);
       bg.add(radio);
     }
-    // добавляем все в контейнер
+    // додаємо все в контейнер
     add(r);
     add(panel);
-    // выводим окно на экран
+    // виводимо вікно на екран
     pack();
     setVisible(true);
   }
