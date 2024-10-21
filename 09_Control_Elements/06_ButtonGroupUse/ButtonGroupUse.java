a3ef73375997bccaeea7c41c3300c11118c5d92b
Rutar Andriy <RutarAndriy@gmail.com>
2024-10-21 10:23:49 +0300

    Added example 06 - Button_Group_Use

--- 09_Control_Elements/06_ButtonGroupUse/ButtonGroupUse.java
+++ 09_Control_Elements/06_ButtonGroupUse/ButtonGroupUse.java
@@ -1,6 +1,6 @@
 // ButtonGroupUse.java
-// Класс ButtonGroup помогает обеспечить
-// эксклюзивный выбор
+// Клас ButtonGroup допомагає забезпечити
+// ексклюзивний вибір
 import javax.swing.*;
 import com.porty.swing.BoxLayoutUtils;
 import java.awt.*;
@@ -9,30 +9,30 @@ public class ButtonGroupUse extends JFrame {
   public ButtonGroupUse() {
     super("ButtonGroupUse");
     setDefaultCloseOperation( EXIT_ON_CLOSE );
-    // создадим горизонтальную панель
-    // с блочным расположением
+    // створюємо горизонтальну панель
+    // із блочним розміщенням
     JPanel bh = BoxLayoutUtils.createHorizontalPanel();
-    // надпись и отступ
-    bh.add(new JLabel("Что Вы предпочитаете:"));
+    // напис і відступ
+    bh.add(new JLabel("Чому ви надаєте перевагу:"));
     bh.add(Box.createHorizontalStrut(12));
-    // несколько выключателей JToggleButton
+    // декілька вимикачів JToggleButton
     JToggleButton b1 = new JToggleButton("Чай", true);
-    JToggleButton b2 = new JToggleButton("Кофе");
+    JToggleButton b2 = new JToggleButton("Кава");
     JToggleButton b3 = new JToggleButton("Лимонад");
-    // добавим все кнопки в группу ButtonGroup
+    // додамо всі кнопки в групу ButtonGroup
     ButtonGroup bg = new ButtonGroup();
     bg.add(b1);
     bg.add(b2);
     bg.add(b3);
-    // добавим все кнопки в контейнер, учтем при
-    // этом рекомендации интерфейса "Metal"
+    // додамо всі кнопки в контейнер, врахуємо при
+    // цьому рекомендації інтерфейсу "Metal"
     bh.add(b1);
     bh.add(Box.createHorizontalStrut(2));
     bh.add(b2);
     bh.add(Box.createHorizontalStrut(2));
     bh.add(b3);
     getContentPane().add(bh);
-    // выводим окно на экран
+    // виводимо вікно на екран
     pack();
     setVisible(true);
   }
