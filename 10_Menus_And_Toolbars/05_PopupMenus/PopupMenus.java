b1352128983f40e582ae916c544a0711709cbb9a
Rutar Andriy <RutarAndriy@gmail.com>
2024-10-22 21:44:24 +0300

    Added example 05 - Popup_Menus

--- 10_Menus_And_Toolbars/05_PopupMenus/PopupMenus.java
+++ 10_Menus_And_Toolbars/05_PopupMenus/PopupMenus.java
@@ -1,5 +1,5 @@
 // PopupMenus.java
-// Работа с всплывающими меню
+// Робота зі спливаючими меню
 import javax.swing.*;
 import java.awt.*;
 
@@ -7,27 +7,27 @@ public class PopupMenus extends JFrame {
   public PopupMenus() {
     super("PopupMenus");
     setDefaultCloseOperation( EXIT_ON_CLOSE );
-    // получаем всплывающее меню
+    // отримуємо спливаюче меню
     JPopupMenu popup = createPopupMenu();
-    // и привязываем к нашей панели содержимого
+    // і прив'язуємо до нашої панелі вмісту
     ((JComponent)getContentPane()).
         setComponentPopupMenu(popup);
-    // "прозрачная" для меню кнопка
+    // "прозора" для меню кнопка
     JButton button = new JButton("Проба пера");
     button.setInheritsPopupMenu(true);
     add(button, "South");
-    // выводим окно на экран
+    // виводимо вікно на екран
     setSize(300, 200);
     setVisible(true);
   }
-  // создаем наше всплывающее меню
+  // створюємо наше спливаюче меню
   private JPopupMenu createPopupMenu() {
-    // создаем само всплывающее меню
+    // створюємо саме спливаюче меню
     JPopupMenu pm = new JPopupMenu();
-    // создаем его пункты
-    JMenuItem good = new JMenuItem("Отлично");
-    JMenuItem excellent = new JMenuItem("Замечательно");
-    // и добавдяем все тем же методом add()
+    // створюємо його пункти
+    JMenuItem good = new JMenuItem("Добре");
+    JMenuItem excellent = new JMenuItem("Чудово");
+    // і додаємо все тим же методом add()
     pm.add(good);
     pm.add(excellent);
     return pm;
