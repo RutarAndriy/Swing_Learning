ce0fa3f6d4a1cfea0f8d2696b030a27993a00990
Rutar Andriy <RutarAndriy@gmail.com>
2024-10-27 12:32:27 +0200

    Added example 04 - List_Selection_Modes

--- 11_Lists/04_ListSelectionModes/ListSelectionModes.java
+++ 11_Lists/04_ListSelectionModes/ListSelectionModes.java
@@ -1,20 +1,20 @@
 // ListSelectionModes.java
-// Различные режимы выделения
+// Різні режими виділення
 import javax.swing.*;
 import java.awt.*;
 
 public class ListSelectionModes extends JFrame {
-  private String[] data = { "Красный", "Синий",
-      "Зеленый", "Желтый", "Белый"};
+  private String[] data = { "Червоний", "Синій",
+      "Зелений", "Жовтий", "Білий"};
   public ListSelectionModes() {
     super("ListSelectionModes");
     setDefaultCloseOperation(EXIT_ON_CLOSE);
-    // заполним модель данными
+    // заповнимо модель даними
     DefaultListModel dlm =
         new DefaultListModel();
     for (String next : data)
       dlm.addElement(next);
-    // три списка с разным типом выделения
+    // три списки з різним типом виділення
     JList list1 = new JList(dlm);
     list1.setSelectionMode(
         ListSelectionModel.SINGLE_SELECTION);
@@ -22,15 +22,15 @@ public class ListSelectionModes extends JFrame {
     list2.setSelectionMode(
         ListSelectionModel.SINGLE_INTERVAL_SELECTION);
     JList list3 = new JList(dlm);
-    // аналогично предыдущему вызову
+    // аналогічно попередньому виклику
     list3.getSelectionModel().setSelectionMode(
         ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
-    // добавляем компоненты
+    // додаємо компоненти
     setLayout(new FlowLayout());
     add(new JScrollPane(list1));
     add(new JScrollPane(list2));
     add(new JScrollPane(list3));
-    // выведем окно на экран
+    // виводимо вікно на екран
     setSize(300, 200);
     setVisible(true);
   }
