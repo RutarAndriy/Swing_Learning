93d614131943a02d92e314a4f13f708b11c39bf0
Rutar Andriy <RutarAndriy@gmail.com>
2024-10-28 19:56:33 +0200

    Added example 12 - Using_Combobox_Model

--- 11_Lists/12_UsingComboBoxModel/UsingComboBoxModel.java
+++ 11_Lists/12_UsingComboBoxModel/UsingComboBoxModel.java
@@ -1,40 +1,40 @@
 // UsingComboBoxModel.java
-// Использование стандартной модели раскрывающихся списков
+// Використання стандартної моделі випадаючих списків
 import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
 
 public class UsingComboBoxModel extends JFrame {
-  // наша стандартная модель
+  // наша стандартна модель
   private DefaultComboBoxModel cbm;
   public UsingComboBoxModel() {
     super("UsingComboBoxModel");
     setDefaultCloseOperation(EXIT_ON_CLOSE);
-    // создаем стандартную модель и наполняем ее данными
+    // створюємо стандартну модель і заповнюємо її даними
     cbm = new DefaultComboBoxModel();
     for (int i=0; i<10; i++) {
-      cbm.addElement("Элемент списка №: " + i);
+      cbm.addElement("Елемент списку №: " + i);
     }
-    // сменяем выбранный элемент
+    // змінюємо вибраний елемент
     cbm.setSelectedItem(cbm.getElementAt(4));
-    // список на основе нашей модели
+    // список на основі нашої моделі
     JComboBox combo = new JComboBox(cbm);
     combo.setMaximumRowCount(5);
-    // стандартная модель позволяет
-    // динамически манипулировать данными
-    JButton add = new JButton("Добавить");
+    // стандартна модель дозволяє
+    // динамічно маніпулювати даними
+    JButton add = new JButton("Додати");
     add.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
-        // случайно выбираем позицию
+        // випадково вибираємо позицію
         int pos = (int) (Math.random()*cbm.getSize());
         cbm.insertElementAt("Новинка!", pos);
       }
     });
-    // добавляем список и кнопку в панель
+    // додаємо список і кнопку на панель
     setLayout(new FlowLayout());
     add(combo);
     add(add);
-    // выводим окно на экран
+    // виводимо вікно на екран
     setSize(400, 200);
     setVisible(true);
   }
