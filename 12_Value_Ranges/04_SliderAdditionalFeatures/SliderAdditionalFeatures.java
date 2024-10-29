ef59a7173f4d1aa6c6aee278b7893890198c60db
Rutar Andriy <RutarAndriy@gmail.com>
2024-10-29 16:05:16 +0200

    Added example 04 - Slider_Additional_Features

--- 12_Value_Ranges/04_SliderAdditionalFeatures/SliderAdditionalFeatures.java
+++ 12_Value_Ranges/04_SliderAdditionalFeatures/SliderAdditionalFeatures.java
@@ -1,5 +1,5 @@
 // SliderAdditionalFeatures.java
-// Дополнительные возможности ползунков
+// Додаткові можливості повзунків
 import javax.swing.*;
 import java.util.*;
 import java.awt.*;
@@ -8,36 +8,36 @@ public class SliderAdditionalFeatures extends JFrame {
   public SliderAdditionalFeatures() {
     super("SliderAdditionalFeatures");
     setDefaultCloseOperation(EXIT_ON_CLOSE);
-    // используем стандартную модель
+    // використовуємо стандартну модель
     BoundedRangeModel model =
         new DefaultBoundedRangeModel(10, 0, 0, 60);
-    // таблица с надписями
+    // таблиця з написами
     Dictionary labels = new Hashtable();
     labels.put(0, new JLabel(
-        "<html><font color=red size=4>Ноль"));
+        "<html><font color=red size=4>Нуль"));
     labels.put(10, new JLabel(
-        "<html><font color=green size=3>Двадцать"));
+        "<html><font color=green size=3>Двадцять"));
     labels.put(50, new JLabel(
-        "<html><font color=yellow size=5>Много"));
+        "<html><font color=yellow size=5>Багато"));
     labels.put(60, new JLabel(
-        new ImageIcon("caution.gif")));
-    // настройка первого ползунка
+        new ImageIcon("Caution.gif")));
+    // налаштування першого повзунка
     JSlider slider1 = new JSlider(JSlider.VERTICAL);
     slider1.setModel(model);
     slider1.setLabelTable(labels);
     slider1.setPaintLabels(true);
-    // "наполняемый" инвертированный ползунок
+    // "наповнюваний" інвертований повзунок
     JSlider slider2 = new JSlider(model);
     slider2.putClientProperty(
         "JSlider.isFilled", Boolean.TRUE);
     slider2.setInverted(true);
     slider2.setPaintTicks(true);
     slider2.setMajorTickSpacing(10);
-    // добавляем компоненты в окно
+    // додаємо компоненти у вікно
     setLayout(new FlowLayout());
     add(slider1);
     add(slider2);
-    // выводим окно на экран
+    // виводимо вікно на екран
     setSize(300, 300);
     setVisible(true);
   }
