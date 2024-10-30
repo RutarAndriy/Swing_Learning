e154d5ba78411daa5bb86c96073f3762f6e4bfdb
Rutar Andriy <RutarAndriy@gmail.com>
2024-10-30 22:58:45 +0200

    Added example 10 - Spinner_Editors

--- 12_Value_Ranges/10_SpinnerEditors/SpinnerEditors.java
+++ 12_Value_Ranges/10_SpinnerEditors/SpinnerEditors.java
@@ -1,35 +1,35 @@
 // SpinnerEditors.java
-// Стандартные редакторы счетчиков
+// Стандартні редактори лічильників
 import javax.swing.*;
 import java.util.*;
 import java.awt.*;
 
 public class SpinnerEditors extends JFrame {
-  // данные для первого счетчика
+  // дані для першого лічильника
   private String[] data = {
-      "Первый", "Второй", "Последний"
+      "Перший", "Другий", "Останній"
   };
   public SpinnerEditors() {
     super("SpinnerEditors");
     setDefaultCloseOperation(EXIT_ON_CLOSE);
-    // счетчик на основе массива
+    // лічильник на основі масиву
     JSpinner spinner1 = new JSpinner(
         new SpinnerListModel(data));
-    // настраиваем редактор
+    // налаштовуємо редактор
     ((JSpinner.ListEditor)spinner1.getEditor()).
         getTextField().setColumns(15);
-    // выбор дат
+    // вибір дат
     SpinnerDateModel dates = new SpinnerDateModel(
         new Date(), null, null, Calendar.DAY_OF_MONTH);
     JSpinner spinner2 = new JSpinner(dates);
-    // настраиваем редактор
+    // налаштовуємо редактор
     ((JSpinner.DateEditor)spinner2.getEditor()).
         getTextField().setEditable(false);
-    // добавляем счетчики в панель содержимого
+    // додаємо лічильники до панелі вмісту
     setLayout(new FlowLayout());
     add(spinner1);
     add(spinner2);
-    // выводим окно на экран
+    // виводимо вікно на екран
     setSize(400, 300);
     setVisible(true);
   }
