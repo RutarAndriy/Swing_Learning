bd5bbc83a46c34ae9120dc8394cc7acb431fd250
Rutar Andriy <RutarAndriy@gmail.com>
2024-10-30 23:21:45 +0200

    Added example 01 - Simple_Tabbed_Panes

--- 13_Space_Management/01_SimpleTabbedPanes/SimpleTabbedPanes.java
+++ 13_Space_Management/01_SimpleTabbedPanes/SimpleTabbedPanes.java
@@ -1,5 +1,5 @@
 // SimpleTabbedPanes.java
-// Использование панелей с вкладками
+// Використання панелей із вкладками
 import javax.swing.*;
 import java.awt.*;
 
@@ -7,31 +7,31 @@ public class SimpleTabbedPanes extends JFrame {
   public SimpleTabbedPanes() {
     super("SimpleTabbedPanes");
     setDefaultCloseOperation(EXIT_ON_CLOSE);
-    // первая панель с вкладками
+    // перша панель із вкладками
     JTabbedPane tabsOne = new JTabbedPane(
         JTabbedPane.BOTTOM, JTabbedPane.SCROLL_TAB_LAYOUT);
-    // добавляем вкладки
+    // додаємо вкладки
     for (int i=1; i<8; i++) {
       JPanel tab = new JPanel();
       tab.add(new JButton("Просто кнопка " + i));
       tabsOne.addTab("Вкладка №: " + i, tab);
     }
-    // вторая панель с вкладками
+    // друга панель із вкладками
     JTabbedPane tabsTwo = new
         JTabbedPane(JTabbedPane.TOP);
-    // добавляем вкладки
+    // додаємо вкладки
     for (int i=1; i<8; i++) {
       JPanel tab = new JPanel();
-      tab.add(new JButton("Снова кнопка " + i));
+      tab.add(new JButton("Знову кнопка " + i));
       tabsTwo.addTab("<html><i>Вкладка №: " + i,
-          new ImageIcon("icon.gif"),
-          tab, "Нажмите " + i + "!");
+          new ImageIcon("Icon.gif"),
+          tab, "Натисніть " + i + "!");
     }
-    // добавляем вкладки в панель содержимого
+    // додаємо вкладки до панелі вмісту
     setLayout(new GridLayout());
     add(tabsOne);
     add(tabsTwo);
-    // выводим окно на экран
+    // виводимо вікно на екран
     setSize(600, 250);
     setVisible(true);
   }
