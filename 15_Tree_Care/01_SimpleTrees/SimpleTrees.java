ab413fdd5abc7f8e71c732f99b20532cf3ab87d3
Rutar Andriy <RutarAndriy@gmail.com>
2024-10-31 17:42:13 +0200

    Added example 01 - Simple_Trees

--- 15_Tree_Care/01_SimpleTrees/SimpleTrees.java
+++ 15_Tree_Care/01_SimpleTrees/SimpleTrees.java
@@ -1,5 +1,5 @@
 // SimpleTrees.java
-// Создание самых простых деревьев
+// Створення найпростіших дерев
 import javax.swing.*;
 import java.util.*;
 import java.awt.*;
@@ -8,30 +8,30 @@ public class SimpleTrees extends JFrame {
   public SimpleTrees() {
     super("SimpleTrees");
     setDefaultCloseOperation(EXIT_ON_CLOSE);
-    // создание дерева на основе массива
-    Object[] data = new Object[] { "Первый", "Второй",
-        "Третий", new String[] { "Чей-то потомок",
-            "Еще потомок" }
+    // створення дерева на основі масиву
+    Object[] data = new Object[] { "Перший", "Другий",
+        "Третій", new String[] { "Чийсь нащадок",
+            "Ще нащадок" }
     };
     JTree tree1 = new JTree(data);
-    // дерево на основе вектора
+    // дерево на основі вектора
     Vector<String> vector = new Vector<String>();
     for (int i=0; i<5; i++) vector.add("Лист № " + i);
     JTree tree2 = new JTree(vector);
-    // дерево на основе таблицы
+    // дерево на основі таблиці
     Hashtable<String,String> table =
         new Hashtable<String,String>();
     table.put("Одна", "пара");
-    table.put("Еще одна", "тоже пара");
+    table.put("Ще одна", "також пара");
     JTree tree3 = new JTree(table);
-    // можно включить показ корня дерева
+    // можна відобразити показ кореня дерева
     tree3.setRootVisible(true);
-    // добавим деревья одно за одним
+    // додамо дерева одне за одним
     setLayout(new FlowLayout());
     add(tree1);
     add(tree2);
     add(tree3);
-    // выводим окно на экран
+    // виводимо віно на екран
     setSize(400, 300);
     setVisible(true);
 
