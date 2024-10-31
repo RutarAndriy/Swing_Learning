ca22f9f1997d20c405fbfe71304236b7baabea44
Rutar Andriy <RutarAndriy@gmail.com>
2024-10-31 15:46:16 +0200

    Added example 03 - Confirm_Dialogs

--- 14_Standard_Dialogs/03_ConfirmDialogs/ConfirmDialogs.java
+++ 14_Standard_Dialogs/03_ConfirmDialogs/ConfirmDialogs.java
@@ -1,62 +1,62 @@
 // ConfirmDialogs.java
-// Стандартные диалоговые окна для получения от
-// пользователя подтверждения совершаемого действия
+// Стандартні діалогові вікна для отримання від
+// користувача підтвердження здійснюваної дії
 import javax.swing.*;
 import java.awt.event.*;
 import java.awt.*;
 
 public class ConfirmDialogs extends JFrame {
-  // значок для одного из сообщений
-  private Icon icon = new ImageIcon("goblet.png");
+  // значок для одного із повідомлень
+  private Icon icon = new ImageIcon("Goblet.png");
   public ConfirmDialogs() {
     super("ConfirmDialogs");
     setDefaultCloseOperation(EXIT_ON_CLOSE);
-    // после щелчков на кнопках выводятся сообщения
-    JButton confirm1 = new JButton("2 и 4 параметра");
+    // після клацання по кнопких відображаються повідомлення
+    JButton confirm1 = new JButton("2 і 4 параметри");
     confirm1.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
-        // диалоговое окно c 4-мя параметрами
+        // діалогове вікно із 4-ма параметрами
         int res = JOptionPane.showConfirmDialog(
-            ConfirmDialogs.this, "Вы этого хотите?",
-            "Вопрос",
+            ConfirmDialogs.this, "Ви цього хочете?",
+            "Питання",
             JOptionPane.YES_NO_CANCEL_OPTION);
-        // простейшие диалоговые окна
+        // найпростіші діалогові вікна
         if ( res == JOptionPane.YES_OPTION )
           JOptionPane.showConfirmDialog(
-              ConfirmDialogs.this, "Точно хотите?");
+              ConfirmDialogs.this, "Точно хочете?");
         if ( res == JOptionPane.NO_OPTION )
           JOptionPane.showConfirmDialog(
               ConfirmDialogs.this,
-              "Значит, не хотите?");
+              "Значить, не хочете?");
       }
     });
-    JButton confirm2 = new JButton("5 параметров");
+    JButton confirm2 = new JButton("5 параметрів");
     confirm2.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
         int res = JOptionPane.showConfirmDialog(
             ConfirmDialogs.this,
-            "Думайте тщательно, итак...",
-            "Может произойти ошибка!",
+            "Думайте ретельно, отже...",
+            "Може статися помилка!",
             JOptionPane.YES_NO_OPTION,
             JOptionPane.ERROR_MESSAGE);
       }
     });
-    JButton confirm3 = new JButton("6 параметров");
+    JButton confirm3 = new JButton("6 параметрів");
     confirm3.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
         int res = JOptionPane.showConfirmDialog(
-            ConfirmDialogs.this, "Вам нравится значок?",
-            "Полностью настроенное окно!",
+            ConfirmDialogs.this, "Вам подобається значок?",
+            "Повністю налаштоване вікно!",
             JOptionPane.YES_NO_OPTION,
             JOptionPane.ERROR_MESSAGE, icon);
       }
     });
-    // добавляем кнопки в окно
+    // додаємо кнопки у вікно
     setLayout(new FlowLayout());
     add(confirm1);
     add(confirm2);
     add(confirm3);
-    // выводим окно на экран
+    // виводимо вікно на екран
     setSize(300, 200);
     setVisible(true);
   }
