e7888856d9468e4c79d1508f04bc2c8396e12a2c
Rutar Andriy <RutarAndriy@gmail.com>
2024-11-02 17:30:23 +0200

    Added example 01 - Simple_Tables

--- 17_Tables/01_SimpleTables/SimpleTables.java
+++ 17_Tables/01_SimpleTables/SimpleTables.java
@@ -1,47 +1,47 @@
 // SimpleTables.java
-// Простые таблицы с помощью удобных конструкторов
+// Прості таблиці з допомогою зручних конструкторів
 import javax.swing.*;
 import java.util.*;
 import java.awt.*;
 
 public class SimpleTables extends JFrame {
-  // данные для таблиц
+  // дані для таблиць
   private Object[][] colors = new String[][] {
-      { "Красный", "Зеленый", "Синий" },
-      { "Желтый", "Оранжевый", "Белый" },
+      { "Червоний", "Зелений", "Синій" },
+      { "Жовтий", "Оранжевий", "Білий" },
   };
-  // названия заголовков столбцов
+  // назви заголовків стовбців
   private Object[] colorsHeader = new String[] {
-      "Цвет", "Еще цвет", "Тоже цвет"
+      "Колір", "Ще колір", "Також колір"
   };
   public SimpleTables() {
     super("SimpleTables");
     setDefaultCloseOperation(EXIT_ON_CLOSE);
-    // несколько простых таблиц
+    // декілька простих таблиць
     JTable table1 = new JTable(colors, colorsHeader);
     JTable table2 = new JTable(5, 5);
-    // таблица на основе вектора, состоящего из векторов
+    // таблиця на основі вектору, який складається із векторів
     Vector<Vector<String>> data =
         new Vector<Vector<String>>();
     Vector<String> row1 = new Vector<String>();
     Vector<String> row2 = new Vector<String>();
-    // вектор с заголовками столбцов
+    // вектор із заголовками стовбців
     Vector<String> columnNames = new Vector<String>();
-    // наполнение данными
+    // наповнення даними
     for (int i=0; i<3; i++) {
-      row1.add("Ячейка 1." + i);
-      row2.add("Ячейка 2." + i);
-      columnNames.add("Столбец #" + i);
+      row1.add("Комірка 1." + i);
+      row2.add("Комірка 2." + i);
+      columnNames.add("Стовбиць #" + i);
     }
     data.add(row1);
     data.add(row2);
     JTable table3 = new JTable(data, columnNames);
-    // добавляем таблицы в панель с тремя рядами
+    // додаємо таблиці в панель із трьома рядами
     setLayout(new GridLayout(3, 1));
     add(new JScrollPane(table1));
     add(new JScrollPane(table2));
     add(table3);
-    // выводим окно на экран
+    // виводимо вікно на екран
     setSize(350, 400);
     setVisible(true);
   }
