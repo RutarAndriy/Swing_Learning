25a480b201104b84dfa37d820985e1c27d07d7cb
Rutar Andriy <RutarAndriy@gmail.com>
2024-11-02 14:16:41 +0200

    Added example 02 - Using_Text_Area

--- 16_Text_Components/02_UsingTextArea/UsingTextArea.java
+++ 16_Text_Components/02_UsingTextArea/UsingTextArea.java
@@ -1,5 +1,5 @@
 // UsingTextArea.java
-// Использование многострочных полей
+// Використання багатострокових полів
 import javax.swing.*;
 import java.awt.*;
 
@@ -7,21 +7,21 @@ public class UsingTextArea extends JFrame {
   public UsingTextArea() {
     super("UsingTextArea");
     setDefaultCloseOperation(EXIT_ON_CLOSE);
-    // создаем пару многострочных полей
+    // створюємо пару багатострокових полів
     JTextArea area1 = new JTextArea(
-        "Многострочное поле", 5, 10);
-    // нестандартный шрифт и табуляция
+        "Багатострокове поле", 5, 10);
+    // нестандартний шрифт і табуляція
     area1.setFont(new Font("Dialog", Font.PLAIN, 14));
     area1.setTabSize(10);
     JTextArea area2 = new JTextArea(15, 10);
-    // параметры переноса слов
+    // параметри переносу слів
     area2.setLineWrap(true);
     area2.setWrapStyleWord(true);
-    // добавим поля в окно
+    // додамо поля у вікно
     setLayout(new FlowLayout());
     add(new JScrollPane(area1));
     add(new JScrollPane(area2));
-    // выводим окно на экран
+    // виводимо вікно на екран
     setSize(400, 300);
     setVisible(true);
   }
