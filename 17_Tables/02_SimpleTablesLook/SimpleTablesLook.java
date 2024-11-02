a72e47ddf752fd35ffac313fc568157ddd571765
Rutar Andriy <RutarAndriy@gmail.com>
2024-11-02 17:43:40 +0200

    Added example 02 - Simple_Tables_Look

--- 17_Tables/02_SimpleTablesLook/SimpleTablesLook.java
+++ 17_Tables/02_SimpleTablesLook/SimpleTablesLook.java
@@ -1,38 +1,38 @@
 // SimpleTablesLook.java
-// Небольшое изменение внешнего вида таблиц
+// Невеликі зміни зовнішнього вигляду таблиць
 import javax.swing.*;
 import java.awt.*;
 
 public class SimpleTablesLook extends JFrame {
-  // данные и заголовки для таблицы
+  // дані та заголовки для таблиць
   private Object[][] data = new String[][] {
-      { "Мощная", "Синий", "Спортивный" },
-      { "Экономичная", "Красный", "Классика" }
+      { "Потужна", "Синій", "Спортивний" },
+      { "Економічна", "Червоний", "Класичний" }
   };
   private Object[] columns = new String[] {
-      "Модель", "Цвет", "Дизайн"
+      "Модель", "Колір", "Дизайн"
   };
   public SimpleTablesLook() {
     super("SimpleTablesLook");
     setDefaultCloseOperation(EXIT_ON_CLOSE);
-    // таблица с разными расстояниями между ячейками
+    // таблиця із різними віддалями між комірками
     JTable table1 = new JTable(data, columns);
-    // настройка расстояний и цветов
+    // налаштування віддалей та кольорів
     table1.setRowHeight(40);
     table1.setIntercellSpacing(new Dimension(10, 10));
     table1.setGridColor(Color.green);
     table1.setShowVerticalLines(false);
-    // таблица с разными цветами
+    // таблиця з різними кольорами
     JTable table2 = new JTable(data, columns);
     table2.setForeground(Color.red);
     table2.setSelectionForeground(Color.yellow);
     table2.setSelectionBackground(Color.blue);
     table2.setShowGrid(false);
-    // добавляем таблицы в панель из двух ячеек
+    // додаємо таблиці в панель із двох комірок
     setLayout(new GridLayout(1, 2, 5, 5));
     add(new JScrollPane(table1));
     add(new JScrollPane(table2));
-    // выводим окно на экран
+    // виводимо вікно на екран
     setSize(600, 200);
     setVisible(true);
   }
