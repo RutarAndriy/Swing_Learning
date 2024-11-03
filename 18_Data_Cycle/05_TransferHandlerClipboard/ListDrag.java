c9622eab4f23549fed9d1e5f1f9c2126f9ade93f
Rutar Andriy <RutarAndriy@gmail.com>
2024-11-03 16:08:07 +0200

    Added example 05 - Transfer_Handler_Clipboard

---
+++ 18_Data_Cycle/05_TransferHandlerClipboard/ListDrag.java
@@ -0,0 +1,52 @@
+// ListDrag.java
+// Перетягування і вставка даних та TransferHandler
+import javax.swing.*;
+import java.awt.*;
+import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.StringSelection;
+import java.awt.datatransfer.Transferable;
+
+public class ListDrag extends JFrame {
+  // об'єкт-адаптер для списків та їхніх даних
+  static class ListTransferHandler extends TransferHandler {
+    private JList list;
+    public ListTransferHandler(JList list) {
+      this.list = list;
+    }
+    @Override
+    public int getSourceActions(JComponent c) {
+      // Дані можуть копіюватися табо переміщуватися
+      return TransferHandler.COPY_OR_MOVE;
+    }
+    @Override
+    public boolean canImport(TransferSupport support) {
+      // підтримується імпорт лише строкових даних
+      return support.isDataFlavorSupported(
+          DataFlavor.stringFlavor);
+    }
+    @Override
+    protected Transferable createTransferable(JComponent c) {
+      // стандартний спосіб адаптувати строку для обміну
+      return new StringSelection(
+          list.getSelectedValue().toString());
+    }
+    @Override
+    public boolean importData(TransferSupport support) {
+      if ( support.isDataFlavorSupported(DataFlavor.stringFlavor) ) {
+        try {
+          // додаємо в модель нову строку на вибрану позицію
+          ((DefaultListModel)list.getModel()).
+              add(list.locationToIndex(
+                  support.getDropLocation().getDropPoint()),
+                  // отримання строки із Transferable
+                  support.getTransferable().
+                      getTransferData(DataFlavor.stringFlavor));
+          return true;
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+      return false;
+    }
+  }
+}
--- 18_Data_Cycle/05_TransferHandlerClipboard/TransferHandlerClipboard.java
+++ 18_Data_Cycle/05_TransferHandlerClipboard/TransferHandlerClipboard.java
@@ -1,5 +1,5 @@
 // TransferHandlerClipboard.java
-// РљРѕРїРёСЂРѕРІР°РЅРёРµ РІ Р±СѓС„РµСЂ СЃ РїРѕРјРѕС‰СЊСЋ TransferHandler
+// Копіювання в буфер із допомогою TransferHandler
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -7,29 +7,29 @@ import java.awt.event.ActionListener;
 public class TransferHandlerClipboard extends JFrame {
   public TransferHandlerClipboard() {
     super("TransferHandlerClipboard");
-    // РІС‹С…РѕРґ РїСЂРё Р·Р°РєСЂС‹С‚РёРё РѕРєРЅР°
+    // вихід при закриванні вікна
     setDefaultCloseOperation(EXIT_ON_CLOSE);
-    // СЃРѕР·РґР°РµРј РјРѕРґРµР»СЊ
+    // створюємо модель
     DefaultListModel model = new DefaultListModel();
-    model.addElement("Р Р°Р·");
-    model.addElement("Р”РІР°");
-    // СЃРїРёСЃРѕРє СЃРѕ СЃС‚СЂРѕРєР°РјРё
+    model.addElement("Раз");
+    model.addElement("Два");
+    // список із рядками
     final JList list = new JList(model);
     list.setTransferHandler(new ListDrag.ListTransferHandler(list));
     list.setDragEnabled(true);
     list.setDropMode(DropMode.INSERT);
     add(new JScrollPane(list));
-    JButton copy = new JButton("РљРѕРїРёСЂРѕРІР°С‚СЊ РІ Р±СѓС„РµСЂ");
+    JButton copy = new JButton("Копіювати в буфер");
     copy.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
-        // РЅРµРѕР±С…РѕРґРёРјРѕ РґР»СЏ СЂР°Р±РѕС‚С‹ РєРѕРјР°РЅРґС‹
+        // необхідно для роботи команди
         e.setSource(list);
-        // РІС‹Р·С‹РІР°РµРј СЃС‚Р°РЅРґР°СЂС‚РЅСѓСЋ РєРѕРјР°РЅРґСѓ РєРѕРїРёСЂРѕРІР°РЅРёСЏ
+        // викликаємо стандартну команду копіювання
         TransferHandler.getCopyAction().actionPerformed(e);
       }
     });
     add(copy, "South");
-    // РІС‹РІРµРґРµРј РѕРєРЅРѕ РЅР° СЌРєСЂР°РЅ
+    // виводимо вікно на екран
     setSize(400, 300);
     setVisible(true);
   }
