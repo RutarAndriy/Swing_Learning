ebe70b4223b4cfe039a37d3d6715c181d60502f3
Rutar Andriy <RutarAndriy@gmail.com>
2024-11-03 13:42:30 +0200

    Added example 02 - Pasting_Clipboard

--- 18_Data_Cycle/02_PastingClipboard/PastingClipboard.java
+++ 18_Data_Cycle/02_PastingClipboard/PastingClipboard.java
@@ -1,5 +1,5 @@
 // PastingClipboard.java
-// Р’СЃС‚Р°РІРєР° РґР°РЅРЅС‹С… РІ СЃРёСЃС‚РµРјРЅС‹Р№ Р±СѓС„РµСЂ РѕР±РјРµРЅР°
+// Вставка даних в системний буфер обміну
 import javax.swing.*;
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -13,30 +13,30 @@ public class PastingClipboard extends JFrame {
   private JTextArea textArea;
   public PastingClipboard() {
     super("PastingClipboard");
-    // РІС‹С…РѕРґ РїСЂРё Р·Р°РєСЂС‹С‚РёРё РѕРєРЅР°
+    // вихід при закриванні вікна
     setDefaultCloseOperation(EXIT_ON_CLOSE);
-    // РґРѕР±Р°РІРёРј РєРЅРѕРїРєСѓ РґР»СЏ РІСЃС‚Р°РІРєРё РґР°РЅРЅС‹С…
-    JButton copyButton = new JButton("РљРѕРїРёСЂРѕРІР°С‚СЊ");
-    // С‚РµРєСЃС‚РѕРІРѕРµ РїРѕР»Рµ РґР»СЏ РїРѕР»СѓС‡РµРЅРёСЏ С‚РµРєСЃС‚Р°
+    // додамо кнопку для вставки даних
+    JButton copyButton = new JButton("Копіювати");
+    // текстове поле для отримання тексту
     textArea = new JTextArea();
     copyButton.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
-        // Р±СѓС„РµСЂ РѕР±РјРµРЅР°
+        // буфер обміну
         Clipboard clipboard = Toolkit.getDefaultToolkit().
             getSystemClipboard();
-        // РІСЃС‚Р°РІРёРј РІ Р±СѓС„РµСЂ РЅР°С€Рё Р°РґР°РїС‚РёСЂРѕРІР°РЅРЅС‹Рµ РґР°РЅРЅС‹Рµ
+        // вставимо у буфер наші адаптовані дані
         clipboard.setContents(new ComplexTransferable(), null);
       }
     });
     add(new JScrollPane(textArea));
     add(copyButton, "South");
-    // РІС‹РІРµРґРµРј РѕРєРЅРѕ РЅР° СЌРєСЂР°РЅ
+    // виведемо вікно на екран
     setSize(400, 300);
     setVisible(true);
   }
-  // РѕР±СЉРµРєС‚-Р°РґР°РїС‚РµСЂ РґР»СЏ РЅР°С€РёС… РґР°РЅРЅС‹С…
+  // об'єкт-адаптер для наших даних
   class ComplexTransferable implements Transferable {
-    // СЃРїРёСЃРѕРє РїРѕРґРґРµСЂР¶РёРІР°РµРјС‹С… С‚РёРїРѕРІ РґР°РЅРЅС‹С…
+    // список підтримуваних типів даних
     private DataFlavor[] supportedTypes = new DataFlavor[]
         { DataFlavor.stringFlavor, DataFlavor.imageFlavor };
     public DataFlavor[] getTransferDataFlavors() {
@@ -48,7 +48,7 @@ public class PastingClipboard extends JFrame {
     public Object getTransferData(DataFlavor flavor)
         throws UnsupportedFlavorException, IOException {
       if ( flavor == DataFlavor.imageFlavor ) {
-        // РµСЃР»Рё Р·Р°РїСЂРѕС€РµРЅРѕ РёР·РѕР±СЂР°Р¶РµРЅРёРµ, РІРµСЂРЅРµРј РєСЂР°СЃРЅС‹Р№ РєРІР°РґСЂР°С‚
+        // якщо запитано зображення, вернемо червоний квадрат
         BufferedImage bi = new
             BufferedImage(100, 100, BufferedImage.TYPE_INT_RGB);
         Graphics g = bi.getGraphics();
@@ -56,16 +56,16 @@ public class PastingClipboard extends JFrame {
         g.fillRect(0, 0, 100, 100);
         return bi;
       } else if ( flavor == DataFlavor.stringFlavor ) {
-        // РµСЃР»Рё Р·Р°РїСЂРѕС€РµРЅР° СЃС‚СЂРѕРєР°, РІРµСЂРЅРµРј РІС‹РґРµР»РµРЅРЅС‹Р№ С‚РµРєСЃС‚
+        // якщо запрошено строку, вернемо виділений текст
         return textArea.getSelectedText();
       }
-      // РёСЃРєР»СЋС‡РµРЅРёРµ РІ СЃР»СѓС‡Р°Рµ РЅРµРёР·РІРµСЃС‚РЅРѕРіРѕ С‚РёРїР° РґР°РЅРЅС‹С…
+      // виняток у випадку невідомого типу даних
       throw new UnsupportedFlavorException(flavor);
     }
   }
   public static void main(String[] args) {
     SwingUtilities.invokeLater(
-        new Runnable() {
-          public void run() { new PastingClipboard(); } });
+      new Runnable() {
+        public void run() { new PastingClipboard(); } });
   }
 }
