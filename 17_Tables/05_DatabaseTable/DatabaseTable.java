c7f7abd195f0e75cd3d5af274a900142d1b96fc6
Rutar Andriy <RutarAndriy@gmail.com>
2024-11-05 22:50:20 +0200

    Update DatabaseTable.java

--- 17_Tables/05_DatabaseTable/DatabaseTable.java
+++ 17_Tables/05_DatabaseTable/DatabaseTable.java
@@ -1,6 +1,6 @@
 // DatabaseTable.java
 // Таблиця, працююча із базою даних за посередництва спеціальної моделі
-// Приклад застарів, тому не працює на нових версіях Java починаючи із 8-ї
+// java -classpath h2-2.3.232.jar DatabaseTable.java
 import java.sql.*;
 import java.awt.*;
 import javax.swing.*;
@@ -9,18 +9,18 @@ import com.porty.swing.*;
 public class DatabaseTable {
   // параметри підключення
   private static String
-      dsn = "jdbc:odbc:Library",
-      uid = "",
+      dsn = "jdbc:h2:./Test",
+      uid = "user",
       pwd = "";
   public static void main(String[] args) throws Exception {
     // ініціалізація JDBC
-    Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+    Class.forName("org.h2.Driver");
     // об'єкт-з'єднання із БД
     Connection conn = DriverManager.getConnection(dsn, uid, pwd);
     Statement st = conn.createStatement();
     // виконуємо запит
     ResultSet rs = st.executeQuery(
-        "select * from readers.csv");
+        "SELECT * FROM USERS");
     // наша модель
     final DatabaseTableModel dbm =
         new DatabaseTableModel(true);
